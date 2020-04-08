43e006ae8829ea82ffcf1520ba4fa1c9c8b46eb0
MarijnMetzlar <marijn_jacob@hotmail.com>
2020-04-08 16:02:12 +0200

    Random code numbers implementation

--- Demo.java
+++ Demo.java
@@ -1,7 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
 class Demo {
+	public static int[] currentMastermindCode = {0, 0, 0, 0};
+
 	public static void main(String[] args)
 	{
-		System.out.println("Hoi2");
-		System.out.println("Hoi3");
+		new Demo().setupRandomCode();
+	}
+
+	void setupRandomCode()
+	{
+		for(int i = 0; i < currentMastermindCode.length; i++)
+		{
+			Random randomNumber = new Random();
+			int x = randomNumber.nextInt(6) + 1;
+			currentMastermindCode[i] = x;
+		}
+
+		System.out.println("Random code is: " + currentMastermindCode[0] + currentMastermindCode[1] 
+		+ currentMastermindCode[2] + currentMastermindCode[3]);
+	}
+
+	void checkUserInput()
+	{
+		Scanner keyboardInput = new Scanner(System.in);
+
+		if(keyboardInput != null)
+			keyboardInput.close();
 	}
 }
