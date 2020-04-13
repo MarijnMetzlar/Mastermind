a8728d38e4ece31562d0afda964201e134a7c117
MarijnMetzlar <marijn_jacob@hotmail.com>
2020-04-13 15:02:36 +0200

    Numbers to letters, quit, more feedback

--- Demo.java
+++ Demo.java
@@ -2,14 +2,15 @@ import java.util.Random;
 import java.util.Scanner;
 
 class Demo {
-	int[] currentMastermindCode = {0, 0, 0, 0};
-	String wrongCodeNumbers = "";
-	String correctCodeNumbers = "";
+	char[] currentMastermindCode = {0, 0, 0, 0};
+	
+	String wrongCodeLetters = "";
+	String correctedCodeLetters = "";
 	String answer;
-	int possibleTurns = 10;
-
+	
+	int possibleTurns = 20;
 	int correctAmount = 0;
-	int inWrongOrder = 0;
+	int inWrongOrderAmount = 0;
 
 	public static void main(String[] args)
 	{
@@ -26,12 +27,14 @@ class Demo {
 		{
 			Random randomNumber = new Random();
 			int x = randomNumber.nextInt(6) + 1;
-			currentMastermindCode[i] = x;
+			currentMastermindCode[i] = getLetter(x);
 		}
-		
 		answer = "" + currentMastermindCode[0] + currentMastermindCode[1] + currentMastermindCode[2] + currentMastermindCode[3];
-		System.out.println("Random code is: " + answer);
 
+		System.out.println("");
+		System.out.println("Welkom bij Mastermind!");
+		System.out.println("Vul vier letters in van a tot f, en raad de code.");
+		//System.out.println("Random code is: " + answer);
 		checkUserInput();
 	}
 
@@ -43,70 +46,94 @@ class Demo {
 	//TS: Het programma moet feedback geven gebasseerd op de input.
 	void checkUserInput()
 	{
-		
 		Scanner keyboardInput = new Scanner(System.in);
-		
 		while(possibleTurns > 0)
 		{
 			String userInput = keyboardInput.nextLine();
-			
-			if(userInput.equals(answer))
+			if(userInput.length() != currentMastermindCode.length) 
 			{
-				System.out.println("Je hebt de code goed!");
-				System.out.println("Bedankt voor het spelen!");
-				possibleTurns = 0;
-				
-				if(keyboardInput != null)
-				keyboardInput.close();
+				if(userInput.equals("q"))
+				{
+					System.out.println("Je hebt het spel verlaten.");
+					System.exit(0);
+				}
+				else
+					System.out.println("Je moet 4 cijfers invoeren!");
 			}
-			else
+			else 
 			{
-				System.out.println("De code klopt niet!");
-				giveFeedback(userInput);
-				possibleTurns--;
+				if(userInput.equals(answer)) 
+				{
+					System.out.println("Je hebt de code goed!");
+					System.out.println("Bedankt voor het spelen!");
+					possibleTurns = 0;
+					if(keyboardInput != null)
+						keyboardInput.close();
+				}
+				else 
+				{
+					System.out.println("De code klopt niet!");
+					resetAmounts();
+					System.out.println("Je hebt " + getCorrectAmount(userInput) + " letters op de goeie plaats!");
+					System.out.println("Je hebt " + getWrongPosAmount(userInput) + " letters op de verkeerde plaats!");
+					possibleTurns--;
+				}
 			}
 		}
 	}
 
-	void giveFeedback(String uInput)
+	//FD: De applicatie moet niks meer hoeven doen met de vorige inputs van de gebruiker.
+	//TS: De variabelen die met de vorige inputs te maken heeft moeten worden reset.
+	void resetAmounts()
 	{
-		//FD: De gebruiker moet te weten krijgen hoeveel cijfers correct zijn via tekst.
-		//TS: De input moet vergeleken worden het antwoord.
-		//TS: Hierbij moet gekeken worden of het antwoord gelijke characters heeft als die van de input.
 		correctAmount = 0;
-		inWrongOrder = 0;
-		wrongCodeNumbers = "";
-		correctCodeNumbers = "";
+		inWrongOrderAmount = 0;
+		wrongCodeLetters = "";
+		correctedCodeLetters = "";
+	}
 
-		for(int i = 0; i < currentMastermindCode.length; i++)
-		{
+	//FD: Zet de willekeurig gegenereerde cijfercode om naar een lettercode.
+	//TS: Een int moet omgezet worden naar een char.
+	//TS: Deze char moet vervolgens via het ASCII tabel omgezet worden naar een letter.
+	char getLetter(int i)
+	{
+		if(i < 0 || i > 6)
+			return '?';
+		else
+			return (char)(96 + i);
+	}
+
+	//FD: De gebruiker moet te weten krijgen hoeveel cijfers correct zijn via tekst.
+	//TS: De input moet vergeleken worden met het antwoord.
+	//TS: Hierbij moet gekeken worden of het antwoord gelijke characters heeft als die van de input.
+	int getCorrectAmount(String uInput)
+	{
+		for(int i = 0; i < currentMastermindCode.length; i++) {
 			char charInput = uInput.charAt(i);
 			char charCode = answer.charAt(i);
-
 			if(charInput == charCode)
-			{
 				correctAmount++;
-			}
-			else
-			{
-				wrongCodeNumbers = wrongCodeNumbers + charInput;
-				correctCodeNumbers = correctCodeNumbers + charCode;
+			else {
+				wrongCodeLetters = wrongCodeLetters + charInput;
+				correctedCodeLetters = correctedCodeLetters + charCode;
 			}
 		}
-		System.out.println("Je hebt " + correctAmount + " cijfers op de goeie plaats!");
+		return correctAmount;
+	}
 
-		//FD: De gebruiker moet te weten krijgen welke cijfers niet op de goede plek zitten, maar wel in de code zitten.
-		//TS: Check of de cijfers die niet kloppen wel in de code zitten.
-		//TS: Sla de foute cijfers op in een array, en check per cijfer in de code of die erin zit.
+	//FD: De gebruiker moet te weten krijgen welke cijfers niet op de goede plek zitten, maar wel in de code zitten.
+	//TS: Check of de cijfers die niet kloppen wel in de code zitten.
+	//TS: Sla de foute cijfers op, en check per cijfer in de code of die erin zit kijkend naar de overige cijfers die niet klopten.
+	int getWrongPosAmount(String uInput)
+	{
 		int wrongAmount = currentMastermindCode.length - correctAmount;
-		for(int j = 0; j < wrongAmount; j++)
-		{
-			char charWrongCodeNumber = wrongCodeNumbers.charAt(j);
+		for(int i = 0; i < wrongAmount; i++) {
+			char charWrongCodeNumber = wrongCodeLetters.charAt(i);
 			String number = "" + charWrongCodeNumber;
-			boolean inTheCode = correctCodeNumbers.contains(number);
+			boolean inTheCode = correctedCodeLetters.contains(number);
 			if(inTheCode)
-				inWrongOrder++;
+				inWrongOrderAmount++;
 		}
-		System.out.println("Je hebt " + inWrongOrder + " cijfers op de verkeerde plaats!");
+		return inWrongOrderAmount;
 	}
 }
